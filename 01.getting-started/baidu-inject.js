9ac0dc3753d9be47fd6392229bfb28eb35f4a6a6
helinjiang <me@helinjiang.com>
2017-10-15 23:46:29 +0800

    feat: 新增 inject

--- 01.getting-started/baidu-inject.js
+++ 01.getting-started/baidu-inject.js
@@ -6,10 +6,10 @@ nightmare.on('console', function (type, msg) {
 });
 
 nightmare.goto('https://www.baidu.com')
-
+    .inject('js', './run-in-client/test-util.js')
     .type('form[action*="/s"] [name=f]', 'nightmare')
     .click('form[action*="/s"] [type=submit]')
-    .wait('#content_left').inject('js', './run-in-client/test-util.js')
+    .wait('#content_left')
     .evaluate(function () {
         return {
             byVar: typeof window.testUtilByVar,
